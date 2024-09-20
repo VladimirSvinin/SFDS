dc089aa55bccf885b2b7c6633a01ef85ed8d7f5f
Vladimir <vovassvi@gmail.com>
2024-09-21 00:46:19 +0700

    commit5

--- guess_number_project/game.py
+++ guess_number_project/game.py
@@ -51,5 +51,6 @@ def score_game(random_predict) -> int:
 
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
-if __name__ == '__main__':
-    score_game(random_predict)
+
+# RUN
+score_game(random_predict)
